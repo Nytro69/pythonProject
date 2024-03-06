4236725f09102b51599d97524ccbd3a348ecfff1
Luis Paz <lpaz8806@gmail.com>
2024-03-06 18:14:10 +0100

    Update data_structures.py

--- exercises/data_structures.py
+++ exercises/data_structures.py
@@ -1,8 +1,17 @@
-def foo():
+# X    0    1    Y
+# 1    0    1    0
+# 0    0    1    0
+# 0    1    1    0
+# 0    0    0    0
+
+# True represents a wall
+# X => (0, 0)
+# Y => (0, 3)
+# out => [(0,0), (0,1), (1,1) ... ]
+
+def solve_maze(map, start, end) -> list:
     """
     This function computes bla bla bla
     :return:
     """
-    print("hello")
-
-foo()
+    pass
