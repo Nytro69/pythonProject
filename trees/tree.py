6961c59a6cf0088832581983766110dd7a430f62
Luis Paz <lpaz8806@gmail.com>
2024-04-10 18:41:14 +0200

    example token, parsing

--- trees/tree.py
+++ trees/tree.py
@@ -9,3 +9,45 @@ class Tree:
         right_height = 0 if self.right is None else self.right.height()
         return 1 + max(left_height, right_height)
 
+
+
+class Token:
+    def __init__(self, token_type, value):
+        self.type = token_type
+        self.value = value
+
+    def __str__(self):
+        return f'[{self.type}: {self.value}]'
+
+
+class Parser:
+    def __init__(self):
+        pass
+
+    def parse(self, tokens):
+        left = self.parse_mult_expr(tokens)
+
+        if len(tokens) == 0:
+            return left
+
+        if not tokens[0].type in ('+', '-'):
+            raise Exception("Invalid token")
+
+        op = tokens.pop(0).value
+
+        right = self.parse_mult_expr(tokens)
+        return Operation(op, left, right)
+
+
+    def parse_mult_expr(self, tokens):
+
+        pass
+
+    def parse_atom(self, tokens):
+        if tokens[0].type == 'number':
+            pass
+            # return Number(int(tokens[0].value))
+
+        pass
+
+
